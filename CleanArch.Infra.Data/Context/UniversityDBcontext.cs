5e6c05194876af67089be6e17dbc7fabc1bafc03
cloon <clooner8133@gmail.com>
2020-03-05 23:15:05 +0330

    Create DB

---
+++ CleanArch.Infra.Data/Context/UniversityDBcontext.cs
@@ -0,0 +1,20 @@
+﻿using CleanArch.Domain1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class UniversityDBcontext : DbContext
+    {
+
+        public UniversityDBcontext(DbContextOptions<UniversityDBcontext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Course> MyProperty { get; set; }
+    }
+}
