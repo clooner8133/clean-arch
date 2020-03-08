6a3a3040e6fb1a923cc325b1509feedac0214d14
cloon <clooner8133@gmail.com>
2020-03-08 12:54:47 +0330

    oi

---
+++ CleanArch.MVC/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanArch.MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            courseService = _courseService;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            CourseViewModel model =_courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
